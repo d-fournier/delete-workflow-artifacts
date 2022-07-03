b8ee2cc1ce8b3316201ecbaf2b60c9cb857ea497
dfournier <donovan.fournier@finalcad.com>
2022-07-04 00:50:13 +0200

    Add dummy test

---
+++ __tests__/main.test.ts
@@ -0,0 +1,7 @@
+import {expect, test} from '@jest/globals'
+
+// shows how the runner will run a javascript action with env / stdout protocol
+test('test runs', () => {
+  // NOOP
+  expect(true)
+})
